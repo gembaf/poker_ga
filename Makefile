63d323d091c5f8daa256323f1013b79af66fd2b6
gembaf <gembaf.github@gmail.com>
2016-05-10 13:48:07 +0900

    GAを実行できるようにする

--- Makefile
+++ Makefile
@@ -4,18 +4,24 @@ TRY = 10000
 
 VISIBLE = 0 # true = 1, false = 0
 
-run: PokerExec Stock.ini
-	./PokerExec $(TRY) Stock.ini $(VISIBLE)
+run: PokerExec Deck.txt
+	./PokerExec $(TRY) Deck.txt $(VISIBLE)
+
+ga: PokerExecGA Deck.txt
+	./PokerExecGA $(TRY) Deck.txt $(VISIBLE)
 
 PokerExec: lib/CardLib.c lib/PokerExec.c lib/Poker.c PokerOpe.c
 	gcc -O3 -Ilib -oPokerExec -DCHNG=$(CHNG) -DTAKE=$(TAKE) lib/CardLib.c lib/Poker.c lib/PokerExec.c PokerOpe.c -Wno-unused-result
 
-Stock.ini: StockInit
-	./StockInit $(TRY) > Stock.ini
+PokerExecGA: lib/CardLib.c lib/PokerExecGA.c lib/Poker.c PokerOpeGA.c
+	gcc -O3 -Ilib -oPokerExecGA -DCHNG=$(CHNG) -DTAKE=$(TAKE) lib/CardLib.c lib/Poker.c lib/PokerExecGA.c PokerOpeGA.c -Wno-unused-result
 
-StockInit: lib/CardLib.c lib/StockInit.c
-	gcc -Ilib -oStockInit lib/CardLib.c lib/StockInit.c
+Deck.txt: DeckInit
+	./DeckInit $(TRY) > Deck.txt
+
+DeckInit: lib/CardLib.c lib/DeckInit.c
+	gcc -Ilib -oDeckInit lib/CardLib.c lib/DeckInit.c
 
 clean:
-	rm -rf PokerExec StockInit Stock.ini
+	rm -rf PokerExec PokerExecGA DeckInit Deck.txt
 
---
+++ lib/PokerExecGA.c
@@ -0,0 +1,362 @@
+//====================================================================
+//  事前処理
+//====================================================================
+
+//--------------------------------------------------------------------
+//  ヘッダファイル
+//--------------------------------------------------------------------
+
+#include <stdio.h>     // 標準優出力
+#include <stdlib.h>    // 標準ライブラリ
+#include <string.h>    // 文字列処理
+#include <time.h>      // 時間関数
+
+#include "Poker.h"
+
+#define DECK 10000
+#define STEP (HNUM * (CHNG+1))   // TAKE数分を余分に取っておく
+
+#define POPSIZE 200
+#define GEN 5000
+#define PROB_C 85
+#define PROB_M 5
+
+//====================================================================
+//  大域宣言
+//====================================================================
+
+typedef struct {
+  int gene[STEP];
+  double fit;
+} Genome;
+
+extern int Trial;                        // トライ回数
+extern char Deck[FILE_NAME_LEN];         // 山札ファイル
+extern int Disp_Mode;                    // 表示モード
+extern int Hand_Value[10];
+extern double Take_Weight[10];
+
+extern int StepCounter;
+int Step[STEP] = {0};
+int UsedFlag[STEP] = {0};
+
+void ga_exec(int deck[][CNUM]);
+Genome ga_stock_exec(int stock[]);
+double poker_exec(int stock[]);
+
+void initialize(Genome parent[], int stock[]);
+void set_fitness(Genome g[], int n, int stock[]);
+
+void crossover(Genome parent[], Genome *c1, Genome *c2);
+void crossover_uniform(Genome *g1, Genome *g2);
+void crossover_double(Genome *g1, Genome *g2);
+
+void mutation(Genome g[], int n);
+
+Genome selection(Genome g[], int n);
+Genome selection_roulette(Genome g[], int n);
+
+void generation_change(Genome parent[], Genome child[]);
+Genome get_elite(Genome g[], int n);
+
+int uniq_rand(int base);
+int myrand(void);
+
+//====================================================================
+//  本体関数
+//====================================================================
+
+int main(int argc, char *argv[])
+{
+  //----  局所宣言
+  int point[GNUM][TAKE];    // 各回得点
+  FILE *fp;                 // 山札へのファイルポインタ
+  int k;
+  int deck[DECK][CNUM];
+
+  //----  コマンド引数の確認
+  if ( argc < 4 ) {
+    puts("ERROR : コマンド引数が不正です。");
+    puts("実行パラメタ : トライ回数 山札ファイル 表示モード");
+    exit(EXIT_FAILURE);
+  }
+
+  //----  コマンド引数の格納
+  Trial = atoi(argv[1]);    // トライ回数
+  strcpy(Deck, argv[2]);    // 山札ファイル
+  Disp_Mode = atoi(argv[3]);     // 表示モード
+
+  //----  トライ回数の確認
+  if ( Trial > GNUM ) {
+    puts("ERROR : トライ回数が多すぎます。");
+    exit(EXIT_FAILURE);
+  }
+
+  //----  山札ファイルのオープンとエラー処理
+  fp = fopen(Deck, "r");
+  if ( fp == NULL ) {
+    puts("ERROR : 山札ファイルの指定が不正です。");
+    exit(EXIT_FAILURE);
+  }
+
+  //----  山札作成
+  for ( k = 0; k < Trial; k++ ) {
+    int stock[CNUM];
+    card_stock(stock, fp);      // 山札の生成
+    arr_copy(deck[k], stock, CNUM);
+  }
+
+  //----  ファイルのクローズ
+  fclose(fp);
+
+  //----  GAの実行
+  ga_exec(deck);
+
+  //----  終了
+  return 0;
+}
+
+//----------------------------------------------------------
+//  GAの実行
+//----------------------------------------------------------
+
+void ga_exec(int deck[][CNUM])
+{
+  int k1, k2;
+  double sum = 0;
+
+  fprintf(stderr, "{\n");
+  for ( k1 = 0; k1 < DECK; k1++ ) {
+    Genome elite;
+    elite = ga_stock_exec(deck[k1]);
+
+    sum *= (double)k1 / (k1+1);
+    sum += elite.fit / (k1+1);
+    printf("%5d : %8.3f %8.3f\n", k1, elite.fit, sum);
+    for ( k2 = 0; UsedFlag[k2] ; k2++ ) { fprintf(stderr, "%d,", Step[k2]); }
+    fprintf(stderr, "\n");
+  }
+  fprintf(stderr, "}\n");
+}
+
+//----------------------------------------------------------
+//  各トライの進行
+//----------------------------------------------------------
+
+Genome ga_stock_exec(int stock[])
+{
+  Genome elite;
+  Genome *parent;
+  int gen = 0;
+  int k;
+
+  parent = (Genome*)malloc(sizeof(Genome)*POPSIZE);
+  initialize(parent, stock);
+
+  for ( gen = 0; gen < GEN; gen++ ) {
+    Genome *child = (Genome*)malloc(sizeof(Genome)*POPSIZE);
+
+    //----  交叉
+    for ( k = 0; k < POPSIZE; k += 2 ) {
+      crossover(parent, &child[k], &child[k+1]);
+    }
+
+    //----  突然変異
+    mutation(child, POPSIZE);
+
+    //----  適合度の計算
+    set_fitness(child, POPSIZE, stock);
+
+    //----  世代交代
+    generation_change(parent, child);
+
+    free(child);
+  }
+
+  elite = get_elite(parent, POPSIZE);
+  free(parent);
+  return elite;
+}
+
+//----------------------------------------------------------
+//  各トライの進行
+//----------------------------------------------------------
+
+double poker_exec(int stock[])
+{
+  //----  局所変数
+  int used[CNUM];             // 捨札配列
+  int us;                     // 捨札数
+  double try_p;               // トライ得点
+  int tk;                     // テイク数
+
+  //----  トライの最中(テイクを繰り返す)
+  us = 0;
+  try_p = 0;
+  for ( tk = 0; tk < TAKE; tk++ ) {
+    try_p += poker_take(stock, tk, used, &us) * Take_Weight[tk];
+  }
+
+  return try_p;
+}
+
+//----------------------------------------------------------
+//  遺伝子の初期化
+//----------------------------------------------------------
+
+void initialize(Genome parent[], int stock[])
+{
+  int k1, k2;
+
+  for ( k1 = 0; k1 < POPSIZE; k1++ ) {
+    for ( k2 = 0; k2 < STEP; k2++ ) { parent[k1].gene[k2] = myrand(); }
+    arr_copy(Step, parent[k1].gene, STEP);
+    StepCounter = 0;
+    parent[k1].fit = poker_exec(stock);
+  }
+  set_fitness(parent, POPSIZE, stock);
+}
+
+//----------------------------------------------------------
+//  適合度の計算
+//----------------------------------------------------------
+
+void set_fitness(Genome g[], int n, int stock[])
+{
+  int k;
+  double sum = 0;
+  double ave;
+  for ( k = 0; k < n; k++ ) {
+    arr_copy(Step, g[k].gene, STEP);
+    StepCounter = 0;
+    g[k].fit = poker_exec(stock);
+  }
+}
+
+//----------------------------------------------------------
+//  選択
+//----------------------------------------------------------
+
+Genome selection(Genome g[], int n)
+{
+  return selection_roulette(g, n);
+}
+
+//----------------------------------------------------------
+//  ルーレット選択
+//----------------------------------------------------------
+
+Genome selection_roulette(Genome g[], int n)
+{
+  double total = 0;
+  double val = 0;
+  double r = frand();
+  int k;
+
+  for ( k = 0; k < n; k++ ) { total += g[k].fit; }
+  for ( k = 0; k < n; k++ ) {
+    val += g[k].fit / total;
+    if ( r < val ) { return g[k]; }
+  }
+  return g[ irand(n) ];
+}
+
+//----------------------------------------------------------
+//  交叉
+//----------------------------------------------------------
+
+void crossover(Genome parent[], Genome *c1, Genome *c2)
+{
+  Genome g1 = selection(parent, POPSIZE);
+  Genome g2 = selection(parent, POPSIZE);
+
+  if ( irand(100) < PROB_C ) { crossover_double(&g1, &g2); }
+  *c1 = g1; *c2 = g2;
+}
+
+//----------------------------------------------------------
+//  二点交叉
+//----------------------------------------------------------
+
+void crossover_double(Genome *g1, Genome *g2)
+{
+  int k;
+  int s1 = irand(STEP);
+  int s2 = irand(STEP);
+
+  if ( s1 > s2 ) { int t = s1; s1 = s2; s2 = s1; }
+
+  for ( k = 0; k < STEP; k++ ) {
+    if ( s1 <= k && k <= s2 ) {
+      int t = g1->gene[k];
+      g1->gene[k] = g2->gene[k];
+      g2->gene[k] = t;
+    }
+  }
+}
+
+//----------------------------------------------------------
+//  突然変異
+//----------------------------------------------------------
+
+void mutation(Genome g[], int n)
+{
+  int k;
+
+  for ( k = 0; k < n; k++ ) {
+    if ( irand(100) < PROB_M ) {
+      int r = irand(STEP);
+      g[k].gene[r] = uniq_rand(g[k].gene[r]);
+    }
+  }
+}
+
+//----------------------------------------------------------
+//  エリートの選別
+//----------------------------------------------------------
+
+Genome get_elite(Genome g[], int n)
+{
+  int k;
+  int s = 0;
+  double max = 0;
+  for ( k = 0; k < n; k++ ) {
+    if ( max < g[k].fit ) { max = g[k].fit; s = k; }
+  }
+  return g[s];
+}
+
+//----------------------------------------------------------
+//  世代交代
+//----------------------------------------------------------
+
+void generation_change(Genome parent[], Genome child[])
+{
+  int k;
+
+  parent[0] = get_elite(parent, POPSIZE);
+  for ( k = 1; k < POPSIZE; k++ ) {
+    parent[k] = selection(child, POPSIZE);
+  }
+}
+
+//----------------------------------------------------------
+//  かぶらない乱数
+//----------------------------------------------------------
+
+int uniq_rand(int base)
+{
+  int r = myrand();
+  while ( base == r ) { r = myrand(); }
+  return r;
+}
+
+//----------------------------------------------------------
+//  -1～4の乱数
+//----------------------------------------------------------
+
+int myrand(void)
+{
+  return irand(6)-1;
+}
+
